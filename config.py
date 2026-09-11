e21832e00874002a2d441ca3781647192037733f
leomm-355 <leomax24407@gmail.com>
2026-09-11 09:54:18 +0630

    Update config.py

--- config.py
+++ config.py
@@ -99,7 +99,7 @@ YOUTUBE_IMG_URL = "https://i.ibb.co/0VnyqcDm/x.jpg"
 SPOTIFY_ARTIST_IMG_URL = "https://i.ibb.co/0VnyqcDm/x.jpg"
 SPOTIFY_ALBUM_IMG_URL = "https://i.ibb.co/0VnyqcDm/x.jpg"
 SPOTIFY_PLAYLIST_IMG_URL = "https://i.ibb.co/0VnyqcDm/x.jpg"
-CUSTOM_THUMB_URL = getenv("CUSTOM_THUMB_URL", "https://files.catbox.moe/agqvg6.jpg")
+CUSTOM_THUMB_URL = getenv("CUSTOM_THUMB_URL", "https://n.uguu.se/lEAsLEik.jpg")
 
 
 def time_to_seconds(time):
